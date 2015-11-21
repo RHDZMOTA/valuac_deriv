df47cef39b9b4667f6bfe19553b8cdb33ed9912d
dannyguerra <dannyguerralcala@gmail.com>
2015-11-21 14:37:12 -0600

    Funciones El Pargo

--- prueb.R
+++ prueb.R
@@ -37,7 +37,8 @@ v_min <- min(rend$Value)
 v_max <- max(rend$Value)
 rango <- v_max - v_min
 delta <- rango / clases
-
+ 
+#histograma de frec, acumulada
 interv <- numeric()
 interv[1] <- v_min
 for(i in 2:(clases+1)){
@@ -61,6 +62,9 @@ inversa <- function(u){
   }
   
   # esto es interpolación para sacar la inversa (o algo así). :)
+  #sólo se tienen algunos puntos del histograma de frec. acumulado (límite), s
+  #si cae dentro del intervalo no sabemos, para eso es interpolación
+  #las y´s (num de deseos) representan valores aleatorios que se distribuyen como el hist de frec. normal.
   m <- (interv[nacho]-interv[nacho-1])/(freq_acum[nacho]-freq_acum[nacho-1])
   b <- interv[nacho-1]-m*freq_acum[nacho-1]
   y <- m*u+b
@@ -77,8 +81,6 @@ for(i in 1:deseos){
 
 plot(1:deseos,y)
 hist(y)
-hist(rend$Value, nclass=clases)
-
 
 # calcular la función objetivo (kernel) -----------------------------------
 # Variables:
@@ -104,8 +106,69 @@ for(i in 1:(n-1)){
   fest <- fest + kernel(rend$Value[i])
 }
 
-
 plot(x,fest,type="l")
 #plot(density(rend$Value))
 
+#Creación de función de probabilidad para la gráfica kernel.
+f<-function(u2){
+ nacho2<-1
+ if (u2>=max(x) | u2<=min(x))
+   prob<-0.00000000000001
+ else {
+   while(u2>=x[nacho2])
+     nacho2<-nacho2+1
+   m <- (fest[nacho2]-fest[nacho2-1])/(x[nacho2]-x[nacho2-1])
+   b <- fest[nacho2-1]-m*x[nacho2-1]
+   y <- m*u2+b
+   prob<-y/sum(fest)
+ }
+  return(prob)
+}
 
+#Creación del histograma de frecuencias y función de probabilidad con el hist.
+freq_rel<-numeric(clases)
+for (j in 1:(n-1)){
+  for (i in 2:length(interv)){
+    if (rend$Value[j]<interv[i] & rend$Value[j]>=interv[i-1]){
+      freq_rel[i-1]<-freq_rel[i-1]+1
+    }
+  }
+}
+freq_rel<-freq_rel/sum(freq_rel)
+
+qu<-function(u3){
+  nacho3<-1
+  if (u3>=max(interv) | u3<=min(interv))
+    prob<-0.000000000000000001
+  else {
+    while(u3>=interv[nacho3])
+      nacho3<-nacho3+1
+#       if(nacho3 >= length(interv))
+#         nacho3 <- length(interv)
+    prob<-freq_rel[nacho3-1]#/sum(datoshrv)
+  }
+  return(prob)
+}
+
+#Selección de nums aleatorios (y). Se eligen los que cumplen alpha, se adecúan más.
+#Los que "pasen la prueba" serán la estimación para el siguiente día.
+
+estimadas <- function(e)#d=un vector de aleatorios, e = ultimo dato
+y_esti<-numeric()
+equis <- runif(deseos)
+for (i in 1:deseos){
+  d<-y[i]
+  e<-rend$Value[n-1]
+  alpha<-(f(d)*qu(e))/(f(e)*qu(d))
+  y_esti[i]<-e+(d-e)*(equis[i]<alpha)
+  
+}
+
+# ce <- numeric()
+# a <- 0
+# for(i in 1:length(y_esti)){
+#   if(is.na(y_esti[i])==T){
+#     a <- a+1
+#     ce[a]  <- i
+#   }
+# }
