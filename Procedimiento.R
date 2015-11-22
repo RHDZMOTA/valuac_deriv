8c2179a8073ae13152e49abc74a9775e0f5c1e7f
dannyguerra <dannyguerralcala@gmail.com>
2015-11-21 23:07:11 -0600

    Proced antes de opciones

---
+++ Procedimiento.R
@@ -0,0 +1,187 @@
+# prueba para MCMC
+library(ggplot2)
+library(Quandl)
+library(reshape2)
+source("funciones.R")
+
+# descarga y obtención de datos -------------------------------------------
+
+# https://www.quandl.com/api/v3/datasets/CURRFX/USDMXN.csv?start_date=1996-03-26&end_date=2012-08-14
+inter_t <- c(toString(as.Date(as.numeric(Sys.Date())-365)),  
+             toString(as.Date(Sys.Date())))
+
+url <- paste("https://www.quandl.com/api/v3/datasets/CURRFX/USDMXN.csv?start_date=",
+             inter_t[1],"&end_date=",inter_t[2])
+download.file(url,"USDMXN.csv")
+usdmxn <-  read.csv(file="USDMXN.csv",header=TRUE,sep=",",na.strings=TRUE)
+usdmxn$Date <- as.Date(usdmxn$Date)
+usdmxn[,1:4] <- usdmxn[nrow(usdmxn):1,1:4]
+
+ggplot(data=usdmxn, aes(x=Date, y=Rate))+geom_line()
+
+# rendimientos logarítmicos
+n <- nrow(usdmxn) 
+rend <- as.data.frame(usdmxn)
+rend$Value[2:n] <- log(usdmxn[2:n,2]/usdmxn[1:(n-1),2])
+rend <- rend[2:n,]
+
+ggplot(data=rend, aes(x=Date, y=Value))+geom_line()
+
+
+# determinación de q ------------------------------------------------------
+# determinación del histograma de frecuencias para rend. log
+
+hist(rend$Value)
+ks.test(rend$Value, pnorm(n-1))
+
+clases <- round(sqrt(n-1))
+v_min <- min(rend$Value) 
+v_max <- max(rend$Value)
+rango <- v_max - v_min
+delta <- rango / clases
+
+#histograma de frec, acumulada
+interv <- numeric()
+interv[1] <- v_min
+for(i in 2:(clases+1)){
+  interv[i] <- interv[i-1] + delta
+}
+
+freq_acum <- numeric()
+for(i in 1:length(interv)){
+  freq_acum[i] <- sum(rend$Value<interv[i])/(n-1)
+}
+
+FA <- cbind(interv,freq_acum)
+plot(interv,freq_acum,type="l")
+
+# determinación de q ------------------------------------------------------
+# determinación del histograma de frecuencias para rend. log
+
+hist(rend$Value)
+ks.test(rend$Value, pnorm(n-1))
+
+clases <- round(sqrt(n-1))
+v_min <- min(rend$Value) 
+v_max <- max(rend$Value)
+rango <- v_max - v_min
+delta <- rango / clases
+
+#histograma de frec, acumulada
+interv <- numeric()
+interv[1] <- v_min
+for(i in 2:(clases+1)){
+  interv[i] <- interv[i-1] + delta
+}
+
+freq_acum <- numeric()
+for(i in 1:length(interv)){
+  freq_acum[i] <- sum(rend$Value<interv[i])/(n-1)
+}
+
+FA <- cbind(interv,freq_acum)
+plot(interv,freq_acum,type="l")
+
+
+deseos <- 20
+y <- numeric()
+for(i in 1:deseos){
+  u <- runif(1)
+  y[i] <- inversa(u, interv, freq_acum)
+}
+
+plot(1:deseos,y)
+hist(y)
+
+
+desv_est <- sd(rend$Value)
+
+h <- (4*desv_est^5/(3*(n-1)))^(1/5)
+
+x <- seq((v_min-desv_est),(v_max+desv_est),rango/10000)
+
+fest <- numeric(length(x))
+for(i in 1:(n-1)){
+  fest <- fest + kernel(rend$Value[i], n, h, x)
+}
+
+plot(x,fest,type="l")
+#plot(density(rend$Value))
+
+#Creación del histograma de frecuencias y función de probabilidad con el hist.
+freq_rel<-numeric(clases)
+for (j in 1:(n-1)){
+  for (i in 2:length(interv)){
+    if (rend$Value[j]<interv[i] & rend$Value[j]>=interv[i-1]){
+      freq_rel[i-1]<-freq_rel[i-1]+1
+    }
+  }
+}
+freq_rel<-freq_rel/sum(freq_rel)
+
+#Generación de simulaciones (trayectorias)
+#El primer paso de las trayectorias depende del último dato real. Los demás dependen del simulado anterior.
+days<-30  #días hasta el vencimiento o pago de proveedores :()
+
+y_esti<-matrix(0, nrow=deseos, ncol=days)
+y_esti[, 1] <- RDRL(rend$Value[n-1],deseos, x, fest, interv, freq_rel, freq_acum)
+s_esti<-matrix(0, nrow=deseos, ncol=days)
+s_esti[, 1] <- usdmxn$Rate[n]*exp(y_esti[, 1])
+for(i in 1:deseos){
+  for(j in 2:days){
+    y_esti[i,j] <- RDRL(y_esti[i,j-1],1, x, fest, interv, freq_rel, freq_acum)
+    s_esti[i,j] <- s_esti[i, j-1]*exp(y_esti[i,j])
+  }
+}
+
+s_esti <- as.data.frame(s_esti)
+
+s_estif <- cbind(rep(usdmxn$Rate[n],deseos), s_esti)
+s_estif <- as.data.frame(s_estif)
+
+s_estiff <- cbind(1:nrow(t(s_estif)), t(s_estif))
+s_estiff <- as.data.frame(s_estiff)
+# gráficas ----------------------------------------------------------------
+
+
+russo <- matrix(NA, nrow=(n+days), ncol = (deseos+2))
+russo[1:n,1] <- usdmxn$Rate
+russo[(n):(n+days),(3:ncol(russo))]<- t(s_estif)
+russo <- as.data.frame(russo)
+
+dd <- as.numeric(usdmxn$Date)
+dd <- c(dd, (dd[n]+1):(dd[n]+days))
+dd <- as.Date(dd)
+dd<-as.data.frame(dd)
+
+russo[, 2] <- dd
+
+#Gráfico de solo las trayectorias
+nombres <- numeric()
+nombres[1] <- 'ID'
+
+for(i in 2:ncol(s_estiff)){
+  nombres[i] <- paste('Trayectoria', i-1)
+}
+
+colnames(s_estiff) <- nombres
+russia <- melt(s_estiff, id.vars = "ID")
+colnames(russia) <- c('ID', 'Trayectorias', 'Valor')
+ggplot(russia,aes(ID,Valor,color=Trayectorias))+geom_line()
+
+#Gráfico de precio original + trayectorias
+nombres2 <- numeric()
+nombres2[1] <- 'Precio original'
+nombres2[2] <- 'ID'
+
+russo[, 2] <- c(1:nrow(russo))
+for(i in 3:(ncol(russo))){
+  nombres2[i] <- paste('Trayectoria', i-2)
+}
+
+colnames(russo) <- nombres2
+
+russa <- melt(russo, id.vars = 'ID')
+colnames(russa) <- c('ID', 'Valores', 'Valor')
+ggplot(russa,aes(ID, Valor, color=Valores))+geom_line()
+
